639ac78bdf3f5bc2a8933a2d01361d681e6d48e2
ML <103840895+MLScripts@users.noreply.github.com>
2022-05-26 00:04:57 +0200

    Update server.lua

--- ML-Restart/server.lua
+++ ML-Restart/server.lua
@@ -1,3 +1,22 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+function kickML ()
+    local xPlayers = ESX.GetPlayers()
+    for i=1, #xPlayers, 1 do
+        local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+        xPlayer.kick("ML: Automatic Restart.")
+    end
+end
+
+
+
 Citizen.CreateThread(function()
     while true do
         TriggerClientEvent('chat:addMessage', -1, {
@@ -14,8 +33,8 @@ Citizen.CreateThread(function()
             template = '<div style="padding: 0.41vw; margin: -0.1vw; background-color: rgba(255, 0, 0, 0.3); border-radius: 3px;"><i class="fas fa-angle-double-right"></i> ML Automatic Restart: {0}</div>',
             args = {"Automatische Restart in 10 seconden"}
         })
-        Wait(10000)
-	  DropPlayer(source, "ML: Automatic Restart.")
+        Wait(10000) 
+	       kickML ()
         	os.exit()
         TriggerClientEvent('chat:addMessage', -1, {
             template = '<div style="padding: 0.41vw; margin: -0.1vw; background-color: rgba(255, 0, 0, 0.3); border-radius: 3px;"><i class="fas fa-angle-double-right"></i> ML Automatic Restart: {0}</div>',
